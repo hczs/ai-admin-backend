35cf3bd25d7bf77b62b34594a956712d0a64db7a
houcheng <houcheng@boco.com.cn>
2022-01-07 10:21:22 +0800

    :bulb: 完善注释

--- business/filter.py
+++ business/filter.py
@@ -1,7 +1,7 @@
 import django_filters
 from django_filters.rest_framework import FilterSet
 
-from business.models import File, Task
+from business.models import File, Task, TrafficStatePredAndEta
 
 
 class FileFilter(FilterSet):
@@ -21,12 +21,12 @@ class FileFilter(FilterSet):
 
 class TaskFilter(FilterSet):
     """
-    文件查询条件过滤
+    任务查询条件过滤
     """
     # 创建时间区间查询
     begin = django_filters.DateTimeFilter(field_name='create_time', lookup_expr='gte')
     end = django_filters.DateTimeFilter(field_name='create_time', lookup_expr='lte')
-    # 文件名模糊查询
+    # 任务名模糊查询
     task_name = django_filters.CharFilter(field_name='task_name', lookup_expr='icontains')
 
     class Meta:
